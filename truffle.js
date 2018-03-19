7a154a53579d30a70e379d0eff0834368e9964f4
Victor Vicente <victor.g.vicente@gmail.com>
2018-03-19 20:25:19 -0300

    Truffle configuration file

---
+++ truffle.js
@@ -0,0 +1,37 @@
+
+//const WalletProvider = require("truffle-hdwallet-provider-privkey");
+//const privKey = require('fs').readFileSync('./infura_privKey').toString();
+//const apiKey = require('fs').readFileSync('./infura_apiKey').toString();
+
+
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gas: 4500000,
+    },
+    mainnet: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '1', // Match any network id
+      gas: 4500000,
+      gasPrice: 10000000000
+    },
+    ropsten: {
+      //provider: new WalletProvider(privKey, "https://ropsten.infura.io/"+ apiKey),
+      host: 'localhost',
+      port: 8545,
+      network_id: '3', // Match any network id
+      gas: 4500000,
+      gasPrice: 10000000000
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+};
